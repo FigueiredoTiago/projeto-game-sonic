cc74aaad9b91584e6a5d8be170ac1385a4e06e74
tiago <tiagoandroloko@gmail.com>
2023-02-01 16:27:45 -0300

    finalizado

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,37 @@
-const mario = document.querySelector('.mario');
+const person = document.querySelector('.person');
+const pipe = document.querySelector('.pipe');
+const restart = document.querySelector('.game-board');
 
+const jump = () => {
+    person.classList.add('jump')
 
+    setTimeout(() => {
 
-document.addEventListener('keypress', () => {
-    mario.classList.toggle('jump')
+        person.classList.remove('jump')
+
+    }, 500);
 }
-)
+
+const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const personPosition = +getComputedStyle(person).bottom.replace('px', '');
+
+    if(pipePosition <= 120 && pipePosition > 0 && personPosition < 55 ) {
+        person.src = 'assets/img/sonicfim.png'
+        
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        person.style.animation = 'none';
+        person.style.bottom = `${personPosition}px`
+
+        restart.classList.add('try-again')
+        
+    }
+
+    
+
+}, 10);
+
+document.addEventListener('keypress', jump
+);
